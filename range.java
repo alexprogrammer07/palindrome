2ce58914d4cfdcfe5eb6c84260c630150152940d
Alex Programmer <63038222+alexprogrammer07@users.noreply.github.com>
2021-10-01 23:44:02 +0530

    Create range.java

---
+++ range.java
@@ -0,0 +1,19 @@
+class PalindromeRange
+{
+    public static void main(String args[])
+    {
+        int rev, num, temp, rem;
+        for(int i=100;i<=200;i++)
+        {
+            num=temp=i;
+            while(num>0)
+            {
+                rem=num%10;
+                rev=rev*10+rem;
+                num=num/10;
+            }
+            if(temp==rev)
+                System.out.println("Palindrome number : "+temp);
+        }
+    }
+}
